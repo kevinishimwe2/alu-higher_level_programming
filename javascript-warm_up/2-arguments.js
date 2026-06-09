136f3d8ccb862da1bf1ae5bcacc6676f0f93da37
kevinishimwe2 <k.ishimwe2@alustudent.com>
2026-06-09 14:36:48 +0300

    sss

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 const argv = process.argv.slice(2);
-argv.forEach((arg, index) => {
-  if (index === 0) {
-    console.log('No argument');
-  } else if (index === 1) {
-    console.log('Argument found');
-  }
-  else {
-    console.log('Arguments found');
-  }
-});
+
+if (argv.length === 0) {
+  console.log('No argument');
+} else if (argv.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
